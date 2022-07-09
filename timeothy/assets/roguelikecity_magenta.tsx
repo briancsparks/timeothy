08ab0c41ffdd5a4eb87004d5795c6f64b82a220a
Brian C Sparks <briancsparks@gmail.com>
2022-07-09 16:24:59 -0700

    read xml

---
+++ timeothy/assets/roguelikecity_magenta.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="roguelikecity_magenta" tilewidth="16" tileheight="16" spacing="1" tilecount="1036" columns="37">
+ <image source="assets/kenney/roguelike-city-pack/spritesheet/roguelikecity_magenta.png" trans="ff00ff" width="628" height="475"/>
+</tileset>
